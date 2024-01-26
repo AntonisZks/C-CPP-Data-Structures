648a8f491e4cfec8059e1b475bd66a64a4be265e
AntonisZks <antoniszks2003@gmail.com>
2024-01-26 20:45:19 +0200

    Implemented some basic methods of the array like Constructor, Destructor, Inserting and Printing.

--- array.hpp
+++ array.hpp
@@ -7,23 +7,63 @@
 
 /* Basic Array Structure */
 template <typename array_t> class Array {
-    private:
-        unsigned int size; // Array size
-        array_t* structure;  // An array_t pointer to implement the actual array
+private:
+    unsigned int size;  // Array size
+    array_t* structure; // An array_t pointer to implement the actual array
+    bool* filled;       // An array that determines whether a sepcific index in the array is filled with data
 
-    public:
-        Array(const unsigned int&); // Constructor
-        ~Array(void);               // Destructor
+public:
+    Array(const unsigned int&);
+    ~Array(void);
+
+    void insertAt(const array_t& data, const unsigned int index);
+
+    template <typename atype> friend std::ostream& operator<<(std::ostream& out, Array<atype>& array);
+    array_t& operator[](unsigned int index);
+    const array_t& operator[](unsigned int index) const;
 };
 
-template <typename array_t> Array<array_t>::Array(const unsigned int& size_): size(size_) {
-    std::cout << "Constructor called" << std::endl;
+/* Array Constructor */
+template <typename array_t> Array<array_t>::Array(const unsigned int& size_): size(size_) 
+{
     this->structure = new array_t[this->size];
+    this->filled = new bool[this->size];
+
+    for (unsigned int i = 0; i < this->size; i++)
+        this->filled[i] = false;
 }
 
-template <typename array_t> Array<array_t>::~Array(void) {
-    std::cout << "Destructor called" << std::endl;
-    delete this->structure;
+/* Array Destructor */
+template <typename array_t> Array<array_t>::~Array(void) 
+{
+    delete[] this->structure;
+    delete[] this->filled;
+}
+
+/* Inserts data to the given index of the array */
+template<typename array_t> void Array<array_t>::insertAt(const array_t& data, const unsigned int index) 
+{
+    this->structure[index] = data;
+    this->filled[index] = true;
+}
+
+/* Prints the contents of the array */
+template <typename array_t> std::ostream& operator<<(std::ostream& out, Array<array_t>& array)
+{
+    out << "[";
+    for (unsigned int i = 0; i < array.size; i++) {
+        array.filled[i] ? out << array.structure[i] : out << "undef";
+        (i < array.size - 1) ? out << ", " : out << "]";
+    }
+    return out;
+}
+
+template <typename array_t> array_t& Array<array_t>::operator[](unsigned int index) {
+    return this->structure[index];
+}
+
+template <typename array_t> const array_t& Array<array_t>::operator[](unsigned int index) const {
+    return this->structure[index];
 }
 
 #endif /* _ARRAY_HEADER_ */
--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main(int argc, char* argv[]) 
 {
-    Array<int>* myArray = new Array<int>(10);
-    delete myArray;
+    Array<int> myArray = Array<int>(10);
+
+    for (unsigned int i = 0; i < 5; i++)
+        myArray[i] = i + 1;
+
+    cout << myArray << endl;
 
     return 0;
 }
